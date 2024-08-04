bc5cd1a3c5cb6d5e9d8d8b6d9a61b0330f8cb9ab
mostafa mobasher <mobashermostafa@gmail.com>
2024-08-04 15:59:46 +0330

    value-objects pt1

--- Clean_arch.Domain/Products/Product.cs
+++ Clean_arch.Domain/Products/Product.cs
@@ -1,36 +1,34 @@
-﻿namespace Clean_arch.Domain.Products
+﻿using Clean_arch.Domain.Shared;
+
+namespace Clean_arch.Domain.Products
 {
     public class Product
     {
         public Guid Id { get; private set; }
         public string Title { get; private set; }
-        public int Price { get; private set; }
+        public Money Price { get; private set; }
 
-        public Product(string title , int price)
+        public Product(string title , Money price)
         {
-            Guard(title, price);
+            Guard(title);
             this.Title = title;
             this.Price = price;
             Id = Guid.NewGuid();
         }
 
 
-        public void Edit(string title, int price)
+        public void Edit(string title , Money price )
         {
-            Guard(title, price);
+            Guard(title);
             this.Title = title;
             this.Price = price;
         }
 
-        private static void Guard(string title, int price)
+        private static void Guard(string title)
         {
             if (string.IsNullOrEmpty(title))
             {
                 throw new ArgumentNullException("title");
-            }
-            if (price < 0)
-            {
-                throw new InvalidOperationException();
             }
         }
     }
---
+++ Clean_arch.Domain/Shared/Money.cs
@@ -0,0 +1,20 @@
+﻿namespace Clean_arch.Domain.Shared
+{
+    public class Money
+    {
+
+        /// <summary>
+        /// Rial
+        /// </summary>
+        public int Value { get;}
+
+        public Money(int rialValue)
+        {
+            if (rialValue < 0)
+            {
+                throw new InvalidDataException();
+            }
+            Value = rialValue;
+        }
+    }
+}
