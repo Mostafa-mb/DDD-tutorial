60d7b71ed129c575f1ce27291172bbc5383e8994
mostafa mobasher <mobashermostafa@gmail.com>
2024-08-14 15:31:40 +0330

    Fluent Assertion

--- Calculator.Unit.Test/ComputingTests.cs
+++ Calculator.Unit.Test/ComputingTests.cs
@@ -1,30 +1,82 @@
-﻿namespace Calculator.Unit.Test
+﻿using FluentAssertions;
+using System.Dynamic;
+
+namespace Calculator.Unit.Test
 {
     public class ComputingTests
     {
+
+        Computing Computing;
+        public ComputingTests()
+        {
+            Computing = new();
+        }
+
         [Fact]
         public void OddOrEven_Should_Return_Odd_When_Input_Is_OddValue()
         {
-            var computing = new Computing();
-            var result = computing.OddOrEven(11);
-            Assert.Equal("Odd", result);
+            var result = Computing.OddOrEven(11);
+            //Assert.Equal("Odd", result);
+            result.Should().Be("Odd");
+
         }
 
-        //[Fact]
-        //public void OddOrEven_Should_Return_Even_When_Input_Is_EvenValue()
-        //{
-        //    var computing = new Computing();
-        //    var result = computing.OddOrEven(10);
-        //    Assert.Equal("Even", result);
-        //}
 
         [Theory]
         [InlineData(10)]
         public void OddOrEven_Should_Return_Even_When_Input_Is_EvenValue2(int value)
         {
-            var computing = new Computing();
-            var result = computing.OddOrEven(10);
-            Assert.Equal("Even", result);
+            var result = Computing.OddOrEven(10);
+            result.Should().Be("Even");
         }
+
+        [Fact] 
+        public void CalculateAge_Should_Return_Zero_When_Birth_LessThan_Zero()
+        {
+            var result = Computing.CalculateAge(-1, 1400);
+            result.Should().Be(0);
+        }
+        
+        [Fact] 
+        public void CalculateAge_Should_Throw_ArgumentException_When_Birth_Or_currentYear_Zero()
+        {
+            var result = new Action(() =>
+            {
+                Computing.CalculateAge(0, 1400);
+            });
+            
+            result.Should().Throw<ArgumentException>();
+        }
+        
+        [Fact] 
+        public void CalculateAge_Should_Calculate_Age()
+        {
+            var result = Computing.CalculateAge(1350, 1400);
+            result.Should().Be(50);
+        }
+
+
+        [Fact]
+        public void IsPrime_false_with_value_lessThan_2()
+        {
+            var result = Computing.IsPrime(1);
+            result.Should().Be(false);
+        }
+
+        [Fact]
+        public void IsPrime_false_When_DevideResult_EqualTo_0()
+        {
+            var result = Computing.IsPrime(6);
+            result.Should().Be(false);
+        }
+        
+        
+        [Fact]
+        public void IsPrime_True_When_DevideResult_Not_EqualTo_0()
+        {
+            var result = Computing.IsPrime(5);
+            result.Should().Be(true);
+        }
+
     }
 }
--- Calculator/Computing.cs
+++ Calculator/Computing.cs
@@ -6,5 +6,38 @@
         {
             return value % 2 == 0 ? "Even" : "Odd";
         }
+
+        public int CalculateAge(int birthDate, int currentYear)
+        {
+            if (birthDate < 0)
+            {
+                return 0;
+            }
+
+            if (birthDate == 0 || currentYear == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return currentYear - birthDate;
+        }
+
+
+        public bool IsPrime(int value)
+        {
+            if(value<2)
+            {
+                return false;
+            }
+            //sqrt of 25 is 5
+            for(int diviser=2; diviser<=Math.Sqrt(value); ++diviser)
+            {
+                if (value % diviser == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
